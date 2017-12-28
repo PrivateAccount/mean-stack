5f216cb40b2ffa3212363b7bf0c743824c53dd86
Andrzej <andrzuk@gmail.com>
2017-12-28 13:33:58 +0100

    Completing application structure.

--- public/app.js
+++ public/app.js
@@ -5,25 +5,19 @@ var myApp = angular.module('myApp', ['ngSanitize']).controller('mainController',
     function ($scope, $http, $sce) {
 
         $scope.getIndexPage = function () {
-            console.log('Request do serwera... getIndexPage');
             $http.get('/index').then(function (response) {
-                console.log('łapiemy:', response);
                 $scope.mainContent = $sce.trustAsHtml(response.data.content);
             });
         };
 
         $scope.showLoginForm = function () {
-            console.log('Request do serwera... showLoginForm');
             $http.get('/login').then(function (response) {
-                console.log('łapiemy:', response);
                 $scope.mainContent = $sce.trustAsHtml(response.data.content);
             });
         };
 
         $scope.showRegisterForm = function () {
-            console.log('Request do serwera... showRegisterForm');
             $http.get('/register').then(function (response) {
-                console.log('łapiemy:', response);
                 $scope.mainContent = $sce.trustAsHtml(response.data.content);
             });
         };
@@ -34,7 +28,20 @@ var myApp = angular.module('myApp', ['ngSanitize']).controller('mainController',
             });
         };
 
+        $scope.getBlogPage = function () {
+            $http.get('/blog').then(function (response) {
+                $scope.mainContent = $sce.trustAsHtml(response.data.content);
+            });
+        };
+
+        $scope.getContactPage = function () {
+            $http.get('/contact').then(function (response) {
+                $scope.mainContent = $sce.trustAsHtml(response.data.content);
+            });
+        };
+
         $scope.getIndexPage();
 
     }
+
 ]);
